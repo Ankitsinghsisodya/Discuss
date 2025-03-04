9087dee629f8b187c00cf38d5b0c9b03b2e3b0cb
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-03-04 09:10:39 +0530

    comment feature added

--- src/components/comments/comment-create-form.tsx
+++ src/components/comments/comment-create-form.tsx
@@ -1,12 +1,50 @@
-import React from 'react'
+'use client'
+import React, { useActionState, useState } from 'react'
 import { Button } from '../ui/button'
+import { Textarea } from '../ui/textarea'
+import { createComment } from '@/actions/create-comment';
+import { Loader2 } from 'lucide-react';
 
-function CommentCreateForm() {
-  return (
-    <div>
-      <Button></Button>
-    </div>
-  )
+type CommentCreateFormProps = {
+    postId: string,
+    parentId?: string
+    startOpen?: boolean
+}
+
+const CommentCreateForm: React.FC<CommentCreateFormProps> = ({ postId, parentId, startOpen }) => {
+    const [open, setOpen] = useState(startOpen);
+    const [formState, action, isPending] = useActionState(createComment.bind(null, { postId, parentId }), { errors: {} });
+    return (
+        <div>
+            <Button
+                size={'sm'}
+                variant={'link'}
+                onClick={() => setOpen(!open)}>Reply</Button>
+            {
+                open && (
+
+                    <form action={action} className='space-y-2'>
+                        <Textarea
+                            name="content"
+                            placeholder='write a comment...'
+                            className='bg-gray-100 focus-visible:ring-0 ' />
+                        {formState.errors.content && <p className="text-sm text-red-600">{formState.errors.content}</p>}
+                        {formState.errors.formError && <div className="text-sm bg-red-200 border border-red-600 p-2 rounded">{formState.errors.formError}</div>}
+                        <Button disabled={isPending} variant={'secondary'} size={"sm"}>
+                            {
+                                isPending ? (
+                                    <>
+                                        <Loader2 />
+                                        Please wait
+                                    </>
+                                ) : "save"
+                            }
+                        </Button>
+                    </form>
+                )
+            }
+        </div>
+    )
 }
 
 export default CommentCreateForm
---
+++ src/components/comments/comment-list.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import CommentShow from './comment-show'
+import { fetchCommentsByPostId } from '@/lib/query/comment'
+import TopicCreateForm from '../topics/TopicCreateForm'
+
+type CommentListProps = {
+    postId: string
+}
+const CommentList: React.FC<CommentListProps> = async ({ postId }) => {
+    const comments = await fetchCommentsByPostId(postId);
+
+    const topLevelComments = comments.filter((comment) => !comment.parentId);
+    return (
+        <div>
+            <h1 className='font-bold text-lg'>All 0 comments</h1>
+            {
+                topLevelComments.map((comment) => (
+
+                    <CommentShow key={comment.id} postId={comment.postId} commentId={comment.id} />
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default CommentList
