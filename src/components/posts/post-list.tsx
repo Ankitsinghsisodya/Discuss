cae1e051ffc75ab03119fb370523ba24cbce72a1
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-03-06 12:46:21 +0530

    updated cloud storage

--- src/components/posts/post-list.tsx
+++ src/components/posts/post-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardDescription, CardHeader, CardTitle } from '../ui/card'
 import { PostWithData } from '@/lib/query/post'
+import Link from 'next/link'
 
 type PostListProps = {
     fetchData: () => Promise<PostWithData[]>
@@ -13,17 +14,19 @@ const PostList: React.FC<PostListProps> = async ({ fetchData }) => {
     return (
         <div className='flex flex-col gap-2'>
             {posts.map((post) => (
-                <Card key={post.id}>
-                    <CardHeader>
-                        <CardTitle>
-                            {post.title}
-                        </CardTitle>
-                        <CardDescription className='flex items-center justify-between'>
-                            <h1>By {post.user.name}</h1>
-                            <h1>{post._count.comments} comments</h1>
-                        </CardDescription>
-                    </CardHeader>
-                </Card>
+                <Link key={post.id} href={`/topics/${post.topic.slug}/posts/${post.id}`}>
+                    <Card >
+                        <CardHeader>
+                            <CardTitle>
+                                {post.title}
+                            </CardTitle>
+                            <CardDescription className='flex items-center justify-between'>
+                                <h1>By {post.user.name}</h1>
+                                <h1>{post._count.comments} comments</h1>
+                            </CardDescription>
+                        </CardHeader>
+                    </Card>
+                </Link>
             ))}
         </div>
     )
